d5d9c63908bdfc4784bc3545757a9efa10d319da
Valeriia <bartfeld.ler@yandex.ru>
2022-03-03 13:17:00 +0200

    Add Task_3

---
+++ Task_3/Program.cs
@@ -0,0 +1,14 @@
+﻿
+// Является ли число чётным?
+int number = new Random().Next(-100,101);
+int del = number%2;
+Console.WriteLine($"Получено случайное число {number}");
+Console.WriteLine($"Является ли число {number} чётным?");
+if (del == 0)
+{
+    Console.WriteLine("Является");
+}
+else
+{Console.WriteLine("Не является");
+}
+
