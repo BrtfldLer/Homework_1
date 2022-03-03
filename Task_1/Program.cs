ce552c1c71b0847c9b1d5dc2c4adc98d28643fee
Valeriia <bartfeld.ler@yandex.ru>
2022-03-03 13:15:49 +0200

    Add Task_1

---
+++ Task_1/Program.cs
@@ -0,0 +1,15 @@
+﻿// Программа на вход принимает два числа и выдаёт какое большее, а какое меньшее.
+
+int A = new Random().Next(-100,101);
+int B = new Random().Next(-100,101);
+Console.WriteLine($"Даны два числа: {A} и {B}");
+if (A>B)
+{
+    Console.WriteLine($"Большее число - {A}");
+    Console.WriteLine($"Меньшее число - {B}");
+}
+else
+{
+    Console.WriteLine($"Большее число - {B}");
+    Console.WriteLine($"Меньшее число - {A}");
+}
