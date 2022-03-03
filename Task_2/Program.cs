5df5848d83c8dc8b1bfc59eceda413a1d6f290fe
Valeriia <bartfeld.ler@yandex.ru>
2022-03-03 13:16:43 +0200

    Add Task_2

---
+++ Task_2/Program.cs
@@ -0,0 +1,19 @@
+﻿// Найти максимальное из трех чисел
+int a = new Random().Next(0,101);
+int b = new Random().Next(0,101);
+int c = new Random().Next(0,101);
+int max = a;
+Console.WriteLine($"Даны три числа: {a}, {b} и {c}");
+if (a > b)
+{
+    max = a;
+}
+else
+{
+    max = b;
+}
+if (max < c)
+{
+    max = c;
+}
+Console.WriteLine($"{max} - максимальное из трёх чисел");
