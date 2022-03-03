8b63b040713024ed42db44ee8a0f92cf2e301eec
Valeriia <bartfeld.ler@yandex.ru>
2022-03-03 13:17:17 +0200

    Add Task_4

---
+++ Task_4/Program.cs
@@ -0,0 +1,16 @@
+﻿// Показать четные числа от 1 до N
+int first = 1;
+Console.WriteLine("Эта программа выведет на экран все чётные числа от единицы до того, которое Вы выберете включительно.");
+Console.WriteLine("Введите последнее число множества (положительное).");
+int last = Convert.ToInt32(Console.ReadLine());
+int between = last - first;
+Console.WriteLine($"Ниже выведены все чётные числа от {first} до {last}");
+int count = 1;
+int i = first + 1;
+Console.Write($"{i} ");
+while (count < between/2)
+{
+    i = i + 2;
+    count = count + 1;
+    Console.Write($"{i} ");
+}
